824338be051ad549b2dd5dff8ea3d790b50e7580
Santhosh Pulipalupula <santhoshpulipalupula@Santhoshs-MacBook-Air.local>
2021-08-08 19:46:20 -0700

    Adding all the required file

---
+++ newProg.py
@@ -0,0 +1,36 @@
+#!/Library/Frameworks/Python.framework/Versions/3.9/bin/python3
+import sys
+
+def sum():
+  x= str(input("Number: " ))
+  temp=0
+  for i in x:
+    if i.isnumeric():
+      temp = temp + int(i)
+    else:
+      continue
+  return(temp)
+
+def file_sum(file):
+  with open(file, "r") as f:
+    for readline in f:
+      x=readline
+      temp=0
+      for i in x:
+        if i.isnumeric():
+          temp = temp + int(i)
+        else:
+          continue
+      return(temp)
+
+def hexnumbers():
+  valid_values= ('A','B','C','D','E','F','a','b','c','d','e','f','0','1','2','3','4','5','6','7','8','9')
+  x= str(input("Number: " ))
+  temp=0
+  for i in x:
+    if i not in valid_values:
+      print('invalid numbers')
+      break
+    y = int(i, 16)
+    temp = temp + int(i, 16)
+  return(temp)
---
+++ test_myprog.py
@@ -0,0 +1,18 @@
+#!/Library/Frameworks/Python.framework/Versions/3.9/bin/python3
+
+import pytest
+import newProg
+from io import StringIO
+
+def test_sum(monkeypatch):
+  input = StringIO('abcd123')
+  monkeypatch.setattr('sys.stdin', input)	
+  assert newProg.sum() == 6
+
+def test_hexnumbers(monkeypatch):
+  input = StringIO('abc123')
+  monkeypatch.setattr('sys.stdin', input)
+  assert newProg.hexnumbers() == 39
+
+def test_file_sum():
+  assert newProg.file_sum('file.tmp') == 6
